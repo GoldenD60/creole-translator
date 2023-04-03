130cc08df621600fd3bff0167d355f37208efd8e
David Skillman <65703529+GoldenD60@users.noreply.github.com>
2023-04-03 13:22:42 +0100

    Commit

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 translator = `
 {
-	"plurals":
+	"nouns":
 	{
-		"englishList":["ants", "cockroaches", "cockerels", "cats", "cows", "dogs", "hens", "goats", "horses", "spiders", "lizards", "moths", "butterflies", "snails", "pigeons", "mynah birds", "black parrots", "slugs", "tortoises", "turtles", "bulls", "sharks", "ducks", "turkeys", "geese", "guinia fowls", "bats", "owls", "grass snakes", "rats"],
-		"creoleList": ["fourmi", "kankrela", "kok", "sat", "bef", "lisyen", "poul", "kabri", "seval", "bib", "lezar", "bigay", "papiyon", "kourpa", "pizon", "martin", "kato nwanr", "lalangmelani", "torti", "tortid'mer", "toro", "reken", "kannar", "denn", "zwa", "pentar", "sov souri", "ibou", "koulev", "lera"]
+		"plurals":
+		{
+			"englishList":["ants", "cockroaches", "cockerels", "cats", "cows", "dogs", "hens", "goats", "horses", "spiders", "lizards", "moths", "butterflies", "snails", "pigeons", "mynah birds", "parrots", "slugs", "tortoises", "turtles", "bulls", "sharks", "ducks", "turkeys", "geese", "guinia fowls", "bats", "owls", "grass snakes", "rats"],
+			"creoleList": ["fourmi", "kankrela", "kok", "sat", "bef", "lisyen", "poul", "kabri", "seval", "bib", "lezar", "bigay", "papiyon", "kourpa", "pizon", "martin", "kato", "lalangmelani", "torti", "tortid'mer", "toro", "reken", "kannar", "denn", "zwa", "pentar", "sov souri", "ibou", "koulev", "lera"]
+		},
+		"non-plurals":
+		{
+			"englishList":["ant", "cockroach", "cockerel", "cat", "cow", "dog", "hen", "goat", "horse", "spider", "lizard", "moth", "butterfly", "snail", "pigeon", "mynah bird", "parrot", "slug", "tortoise", "turtle", "bull", "shark", "duck", "turkey", "goose", "guinia fowl", "bat", "owl", "grass snake", "rat"],
+			"creoleList": ["fourmi", "kankrela", "kok", "sat", "bef", "lisyen", "poul", "kabri", "seval", "bib", "lezar", "bigay", "papiyon", "kourpa", "pizon", "martin", "kato", "lalangmelani", "torti", "tortid'mer", "toro", "reken", "kannar", "denn", "zwa", "pentar", "sov souri", "ibou", "koulev", "lera"]
+		}
 	},
-	"non-plurals":
+	"adjectives":
 	{
-		"englishList":["ant", "cockroach", "cockerel", "cat", "cow", "dog", "hen", "goat", "horse", "spider", "lizard", "moth", "butterfly", "snail", "pigeon", "mynah bird", "black parrot", "slug", "tortoise", "turtle", "bull", "shark", "duck", "turkey", "goose", "guinia fowl", "bat", "owl", "grass snake", "rat"],
-		"creoleList": ["fourmi", "kankrela", "kok", "sat", "bef", "lisyen", "poul", "kabri", "seval", "bib", "lezar", "bigay", "papiyon", "kourpa", "pizon", "martin", "kato nwanr", "lalangmelani", "torti", "tortid'mer", "toro", "reken", "kannar", "denn", "zwa", "pentar", "sov souri", "ibou", "koulev", "lera"]
+		"englishList":["black"],
+		"creoleList":["nwanr"]
 	}
-}`
+}
+`
 translator = JSON.parse(translator)
 
 english = null
 creole = null
+translated = null
 
 function load() {
 	english = document.getElementById("english")
@@ -31,10 +41,17 @@ function updateCreole() {
 	.replace(/\,/g, " , ")
 	.replace(/\!/g, " ! ")
 	.replace(/\?/g, " ? ")
-	
-	for (var i = 0; i < translator["plurals"]["englishList"].length; i++) {
-		translated = translated.replace(" " + translator["plurals"]["englishList"][i] + " ", " " + translator["plurals"]["creoleList"][i] + " ")
-		translated = translated.replace(" " + translator["non-plurals"]["englishList"][i] + " ", " " + translator["non-plurals"]["creoleList"][i] + " ")
+
+	for (var i = 0; i < translator["adjectives"]["englishList"].length; i++) {
+		translator["adjectives"]["englishList"][i]
+	}
+
+	for (var i = 0; i < translator["nouns"]["plurals"]["englishList"].length; i++) {
+		console.log(`The noun is ${translated.includes(translator["nouns"]["plurals"]["englishList"][i]) || translated.includes(translator["nouns"]["non-plurals"]["englishList"][i]) ? translator["nouns"]["non-plurals"]["englishList"][i] : "nothing"}`)
+		translated = translated.replace(new RegExp(" " + translator["nouns"]["plurals"]["englishList"][i] + " ", "g"), " " + translator["nouns"]["plurals"]["creoleList"][i] + " ")
+		translated = translated.replace(new RegExp(" " + translator["nouns"]["non-plurals"]["englishList"][i] + " ", "g"), " " + translator["nouns"]["non-plurals"]["creoleList"][i] + " ")
+		translated = translated.replace(new RegExp(" " + translator["nouns"]["plurals"]["englishList"][i] + " ", "g"), " " + translator["nouns"]["plurals"]["creoleList"][i] + " ")
+		translated = translated.replace(new RegExp(" " + translator["nouns"]["non-plurals"]["englishList"][i] + " ", "g"), " " + translator["nouns"]["non-plurals"]["creoleList"][i] + " ")
 	}
 
 	// re-add punctuation
@@ -64,8 +81,9 @@ function updateEnglish() {
 	.replace(/\!/g, " ! ")
 	.replace(/\?/g, " ? ")
 	
-	for (var i = 0; i < translator["non-plurals"]["englishList"].length; i++) {
-		translated = translated.replace(" " + translator["non-plurals"]["creoleList"][i] + " ", " " + translator["non-plurals"]["englishList"][i] + " ")
+	for (var i = 0; i < translator["nouns"]["non-plurals"]["englishList"].length; i++) {
+		translated = translated.replace(new RegExp(" " + translator["nouns"]["non-plurals"]["creoleList"][i] + " ", "g"), " " + translator["nouns"]["non-plurals"]["englishList"][i] + " ")
+		translated = translated.replace(new RegExp(" " + translator["nouns"]["non-plurals"]["creoleList"][i] + " ", "g"), " " + translator["nouns"]["non-plurals"]["englishList"][i] + " ")
 	}
 
 	// re-add punctuation
